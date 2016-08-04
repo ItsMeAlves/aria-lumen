d16fca8e134047880bc0ff9b2ca7dfb55d3c9fdb
Gabriel Alves <itsmealves@gmail.com>
2016-08-04 10:26:46 -0300

    remove option parser

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 var config = require("./config/express");
 var routes = require("./routes")(config.app);
 var io = require("socket.io")(config.http);
-var optionParser = require("./config/optionParser");
+var lumen = require("./config/lumen")(io);
 
-optionParser({
-    'just-aria': {
-        truthy: value => {
-            console.log("Sleep, little lumen! Just Aria is needed this time...");
-            io.on("connection", socket => {
-                io.emit("arise");
-            });
-        },
-        falsy: () => {
-            console.log("Alright, Lumen! Wake up!");
-            var lumen = require("./config/lumen")(io);
-        }
-    }
-});
-
+console.log("Alright, Lumen! Wake up!");
 
 config.http.listen(config.app.get("PORT"), function(){
     console.log("listening " + config.app.get("PORT"));
