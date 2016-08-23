06e417199365424567cd7d7b163f2b84d064ae7b
Gabriel Alves <itsmealves@gmail.com>
2016-08-23 16:36:45 -0300

    comment over server file

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var config = require("./config/express");
-var routes = require("./routes")(config.app);
-var io = require("socket.io")(config.http);
-var lumen = require("./config/lumen")(io);
+/*
+ * Aria/Lumen Server
+ */
 
-console.log("Alright, Lumen! Wake up!");
+var config = require("./config/express");     // Configure express router
+var routes = require("./routes")(config.app); // Configure routes
+var io = require("socket.io")(config.http);   // Open Socket io
+var lumen = require("./config/lumen")(io);    // Open Lumen
 
+console.log("Alright, Lumen! Wake up!");      // Log something to the console
+
+//Starts to listen to any request made
 config.http.listen(config.app.get("PORT"), function(){
     console.log("listening " + config.app.get("PORT"));
 });
